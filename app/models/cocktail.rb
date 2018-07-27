eb3569a0991001b5a79c9c307b78aeb3cedfa004
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2018-07-27 10:14:13 -0300

    Defines a pallet to improves css style, adds footer, changes navbar

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,6 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
+  validates :image_url, presence: true
   accepts_nested_attributes_for :doses
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2018_07_26_125131) do
 
   create_table "cocktails", force: :cascade do |t|
     t.string "name"
+    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
