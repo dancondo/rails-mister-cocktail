09a3d1056d294a731a9eec30dacd4412f538db47
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2018-07-27 13:27:26 -0300

    Solve the rollback bug

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,7 +2,7 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
-  validates :image_url, presence: true
+  validates :photo, presence: true
   accepts_nested_attributes_for :doses
   mount_uploader :photo, PhotoUploader
 end
